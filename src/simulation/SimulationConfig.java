c357cc4a4b6262ca69446ebea8784fd4c08e33a9
James Rumsey <jpr31@duke.edu>
2020-01-30 12:01:55 -0500

    implemented buildDocument and readTags as methods (refactored from main)

--- src/simulation/SimulationConfig.java
+++ src/simulation/SimulationConfig.java
@@ -10,54 +10,58 @@ import java.util.List;
 
 public class SimulationConfig {
 
+    private File myFile;
     private int height;
     private int width;
     private String simType;
     private String neighborType;
     private List<String> initialCells;
 
+    public SimulationConfig(File path){
+        //myFile = path;
+        myFile = new File("data/simulation_sample.xml");
+        buildDocument();
+    }
 
-    public static void main(String[] args){
+    private void buildDocument(){
         try {
-            //creating a constructor of file class and parsing an XML file
-            File file = new File("data/simulation_sample.xml");
             //an instance of factory that gives a document builder
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             //an instance of builder to parse the specified xml file
             DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(file);
+            Document doc = db.parse(this.myFile);
             doc.getDocumentElement().normalize();
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
             NodeList nodeList = doc.getElementsByTagName("SimulationConfig");
             // nodeList is not iterable, so we are using for loop
-            for (int itr = 0; itr < nodeList.getLength(); itr++)
-            {
-                Node node = nodeList.item(itr);
-                System.out.println("\nNode Name :" + node.getNodeName());
-                if (node.getNodeType() == Node.ELEMENT_NODE)
-                {
-                    Element eElement = (Element) node;
-                    int height = Integer.parseInt(eElement.getElementsByTagName("height").item(0).getTextContent());
-                    int width = Integer.parseInt(eElement.getElementsByTagName("width").item(0).getTextContent());
-                    String cellType = eElement.getElementsByTagName("type").item(0).getTextContent();
-                    String neighborType = eElement.getElementsByTagName("neighborType").item(0).getTextContent();
-                    System.out.println(height + " " + width + " " + cellType + " " + neighborType);
-
-                    // All initial cell states
-                    NodeList cellList = eElement.getElementsByTagName("cell");
-                    for(int cellItr = 0; cellItr < cellList.getLength(); cellItr++){
-                        Element initialCell = (Element) cellList.item(cellItr);
-                        System.out.println(initialCell.);
-
-                    }
-
-
-
-                }
-            }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void readTags(NodeList nodeList){
+        for (int itr = 0; itr < nodeList.getLength(); itr++) {
+            Node node = nodeList.item(itr);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
+                this.height = Integer.parseInt(eElement.getElementsByTagName("height").item(0).getTextContent());
+                this.width = Integer.parseInt(eElement.getElementsByTagName("width").item(0).getTextContent());
+                this.simType = eElement.getElementsByTagName("type").item(0).getTextContent();
+                this.neighborType = eElement.getElementsByTagName("neighborType").item(0).getTextContent();
+
+                // Initial cell states
+                NodeList cellList = eElement.getElementsByTagName("cell");
+                for(int cellItr = 0; cellItr < cellList.getLength(); cellItr++){
+                    Element initialCell = (Element) cellList.item(cellItr);
+                    initialCells.add(initialCell.getTextContent());
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args){
+
+
     }
 }
