b8f8fda24212bb934372da9ff5dbe246fddabf76
James Rumsey <jpr31@duke.edu>
2020-02-03 12:08:36 -0500

    Began implementation of GridGenerator (xml generator)

---
+++ src/simulation/GridGenerator.java
@@ -0,0 +1,59 @@
+package simulation;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+/**
+ * Based on LevelGenerator.java by
+ * @author Max Smith
+ */
+public abstract class GridGenerator {
+
+
+    protected final int UNIQUE_ID =  new Random().nextInt(100) + 1;;
+    protected String SIM_TYPE;
+    protected int myHeight;
+    protected int myWidth;
+
+    public GridGenerator(int height, int width) throws IOException {
+        myHeight = height;
+        myWidth = width;
+        createFile();
+    }
+
+    public abstract String getTypeString();
+    public abstract String getNeighborString();
+
+    private String nameFile() throws IOException {
+        String fileName = "data/" + SIM_TYPE + UNIQUE_ID + ".xml";
+        File file = new File(fileName);
+        file.createNewFile();
+        return fileName;
+    }
+
+    private void createFile() throws IOException {
+        PrintWriter writer = new PrintWriter(nameFile());
+        writer.println("<?xml version=\"1.0\"?>");
+        writer.println("<SimulationConfig>\n");
+        writer.printf("\t<height>%d</height>\n\t<width>%d</width>\n", myHeight, myWidth);
+        writer.printf("\t<type>%s</type>\n\t<neighborType>%s</neighborType>\n", getTypeString(), getNeighborString());
+        generateCells(writer);
+        writer.println("</SimulationConfig>");
+        writer.close();
+    }
+
+    private void generateCells(PrintWriter w){
+        for(int r = 0; r < myHeight; r++){
+            for(int c = 0; c < myWidth; c++){
+
+            }
+        }
+
+    }
+
+
+
+}
